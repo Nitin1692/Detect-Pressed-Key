45333ef4a5f1a0e628ca2af0f282698d81d78a48
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-02 19:47:34 +0530

    Made changes in both html and javascript file

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const box = document.querySelector(".box")
 document.addEventListener("keydown", e =>{
+        box.querySelector(".key-code").innerText = e.keyCode;
+        box.querySelector(".key-name").innerText = e.key;
+        box.querySelector(".key span").innerText = e.key;
+        box.querySelector(".code span").innerText = e.keyCode;
         box.classList.add("active");
         console.log(e);
 });
